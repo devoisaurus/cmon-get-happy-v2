2653231f4fccbec7a1c5f5ade0631996c5994093
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-24 20:26:10 -0500

    navbar

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 var app = angular.module("GetHappy", []);
 
+app.controller("NavCtrl", function($scope){
+	$scope.navItems = [{name:"Logout"}, {name:"See all cards"}, {name:"See my cards"}, {name:"Add a card"}, ]
+});
+
 app.controller("GetHappyCtrl", function($scope){
 	$scope.welcome = "hello";
 
