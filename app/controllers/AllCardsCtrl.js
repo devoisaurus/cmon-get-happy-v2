005477c2cd2fb488268db99fc309298e5bb98010
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-25 00:15:35 -0500

    firebase get and post

--- app/controllers/AllCardsCtrl.js
+++ app/controllers/AllCardsCtrl.js
@@ -1,31 +1,12 @@
-app.controller("AllCardsCtrl", function($scope){
-$scope.activities = [
-		{
-			id: 0,
-			name: "Take a walk",
-			description: "Take a walk in the park",
-			cost: "$",
-			time: "10 minutes",
-			location: "outdoors",
-			public: true
-		},
-		{
-			id: 1,
-			name: "declutter",
-			description: "Remove 3 things",
-			cost: "$",
-			time: "10 minutes",
-			location: "indoors",
-			public: true
-		},
-		{
-			id: 2,
-			name: "test",
-			description: "words",
-			cost: "$$",
-			time: "time",
-			location: "in out",
-			public: true
-		}
-	];
+app.controller("AllCardsCtrl", function($scope, $http){
+	$scope.activities = [];
+
+	$http.get("https://cmon-get-happy.firebaseio.com/activities.json")
+	.success(function(cardObject){
+		var cardList = cardObject;
+		Object.keys(cardList).forEach(function(key){
+			cardList[key].id=key;
+			$scope.activities.push(cardList[key]);
+		})
+	});
 });
--- app/controllers/NewCardCtrl.js
+++ app/controllers/NewCardCtrl.js
@@ -1,41 +1,27 @@
-app.controller("NewCardCtrl", function($scope){
-	$scope.newCard = {};
-	$scope.activities = [
-		{
-			id: 0,
-			name: "Take a walk",
-			description: "Take a walk in the park",
-			cost: "$",
-			time: "10 minutes",
-			location: "outdoors",
-			public: true
-		},
-		{
-			id: 1,
-			name: "declutter",
-			description: "Remove 3 things",
-			cost: "$",
-			time: "10 minutes",
-			location: "indoors",
-			public: true
-		},
-		{
-			id: 2,
-			name: "test",
-			description: "words",
-			cost: "$$",
-			time: "time",
-			location: "in out",
-			public: true
-		}
-	];
+app.controller("NewCardCtrl", function($scope, $http, $location){
+	$scope.newCard = {
+		cost: "",
+		description: "",
+		location: "",
+		name: "",
+		public: true,
+		time: ""
+	};
 
 	$scope.addNewCard = function(){
-	$scope.newCard.public = true;
-	$scope.newCard.id = $scope.activities.length;
-	console.log("you added a new card", $scope.newCard);
-	$scope.activities.push($scope.newCard);
-	$scope.newCard="";
-}
-
+	$http.post(
+		"https://cmon-get-happy.firebaseio.com/activities.json",
+		JSON.stringify({
+			cost: $scope.newCard.cost,
+			description: $scope.newCard.description,
+			location: $scope.newCard.location,
+			name: $scope.newCard.name,
+			time: $scope.newCard.time,
+			public: true
+		})
+		).success(function(response){
+			console.log(response);
+			$location.url("/cards/all")
+		})
+	};
 });
