c7e1ec40c15d819fe8be334a791e0c28267269d7
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-25 00:33:50 -0500

    firebase delete works

--- app/controllers/AllCardsCtrl.js
+++ app/controllers/AllCardsCtrl.js
@@ -1,12 +1,25 @@
 app.controller("AllCardsCtrl", function($scope, $http){
 	$scope.activities = [];
-
-	$http.get("https://cmon-get-happy.firebaseio.com/activities.json")
-	.success(function(cardObject){
-		var cardList = cardObject;
-		Object.keys(cardList).forEach(function(key){
-			cardList[key].id=key;
+	var getCards = function(){
+		$http.get("https://cmon-get-happy.firebaseio.com/activities.json")
+		.success(function(cardObject){
+				var cardList = cardObject;
+				Object.keys(cardList).forEach(function(key){
+						cardList[key].id=key;
 			$scope.activities.push(cardList[key]);
 		})
 	});
+}
+
+getCards();
+	$scope.cardDelete = function(cardId){
+		console.log("cardId", cardId);
+		$http
+			.delete(`https://cmon-get-happy.firebaseio.com/activities/${cardId}.json`)
+			.success(function(response){
+				console.log(response);
+				$scope.activities=[];
+				getCards();
+			});
+	};
 });
