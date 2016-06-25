5f13f6bd9339bb77509b7a8719f3ce7752542cbf
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-24 23:39:07 -0500

    routing

--- app/app.js
+++ app/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("GetHappy", []);
+var app = angular.module("GetHappy", ["ngRoute"]);
+
+app.config(function($routeProvider){
+	$routeProvider.
+		when('/cards/all', {
+			templateUrl: 'partials/all-cards.html',
+			controller: "GetHappyCtrl"
+		}).
+		when('/cards/add', {
+			templateUrl: 'partials/add-card.html',
+			controller: "GetHappyCtrl"
+		}).
+		when('/cards/user', {
+			templateUrl: 'partials/my-cards.html',
+			controller: "GetHappyCtrl"
+		}).
+		otherwise('cards/all');
+});
