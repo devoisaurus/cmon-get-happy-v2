fc191ab66a4d11aca07676d2939e9c51e2b272cc
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-24 21:32:57 -0500

    buttons and lists

--- app/app.js
+++ app/app.js
@@ -1,12 +1,45 @@
 var app = angular.module("GetHappy", []);
 
 app.controller("NavCtrl", function($scope){
-	$scope.navItems = [{name:"Logout"}, {name:"See all cards"}, {name:"See my cards"}, {name:"Add a card"}, ]
+	$scope.navItems = [{name:"Logout"}, {name:"See all cards"}, {name:"See my cards"}, {name:"Add a card"}, ];
 });
 
 app.controller("GetHappyCtrl", function($scope){
 	$scope.welcome = "hello";
-	$scope.showListView = true;
+	$scope.showListView = false;
+	$scope.card="";
+	$scope.newCard = {};
+
+	$scope.activities = [
+	{
+		id: 0,
+		cost: "$",
+		description: "Take a walk in the park",
+		location: "outdoors",
+		name: "Take a walk",
+		public: true,
+		time: "10 minutes"
+},
+	{
+		id: 1,
+		cost: "$",
+		description: "Remove 3 things",
+		location: "indoors",
+		name: "declutter",
+		public: true,
+		time: "10 minutes"
+	},
+	{
+		id: 3,
+		cost: "$$",
+		description: "words",
+		location: "in out",
+		name: "trst",
+		public: true,
+		time: "time"
+	}
+];
+
 
 	$scope.newCard = function(){
 		console.log("you clicked new card");
