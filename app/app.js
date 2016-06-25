5f28f864ebd658490bff8a0b8475f011ba5d8008
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-24 21:16:54 -0500

    buttons

--- app/app.js
+++ app/app.js
@@ -6,5 +6,16 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("GetHappyCtrl", function($scope){
 	$scope.welcome = "hello";
+	$scope.showListView = true;
+
+	$scope.newCard = function(){
+		console.log("you clicked new card");
+	$scope.showListView = false;
+};
+
+$scope.allCards = function(){
+	console.log("you clicked all cards");
+	$scope.showListView = true;
+};
 
 });
